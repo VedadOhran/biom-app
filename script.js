e74567abb1ee119bfa5662eb3e2c2f74102bdc3d
Vedad <101746711+VedadOhran@users.noreply.github.com>
2022-07-01 20:34:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -22,9 +22,57 @@ const meetBiom = document.querySelector('.meet-biom__container');
   } console.log(meetBiomTopSection)
     }
     
-    window.addEventListener('scroll',verticalScroll)
+    window.addEventListener('scroll',verticalScroll);
 
 
+   //Reviews slider
+
+    const reviews = document.querySelectorAll('.single-review');
+    const dots = document.querySelectorAll('.dot');
+    const current = 0;
+   
+
+    function init(n) {
+  reviews.forEach((review, index) => {
+    review.style.display = 'none';
+    dots.forEach((dot, index) => {
+      dot.classList.remove('active');
+    });
+  });
+  reviews[n].style.display = 'block';
+  dots[n].classList.add('active');
+}
+
+  dots.forEach((dot,index) =>{
+    dot.addEventListener('click', () => {
+      init(index)
+      current = i;
+    })
+  })
+
+
+  const horizontalSection = document.querySelector('.horizontal-scroll');
+  const horizontalScroll = document.querySelector('.horizontal-scroll__container');
+
+  let distance = 0;
+
+  distance = horizontalScroll.scrollWidth;
+
+  horizontalSection.style.height = `${distance}px`;
+ 
+
+  window.addEventListener('scroll', () =>{
+    let vwTop = horizontalSection.getBoundingClientRect().top;
+    
+    if(vwTop < 0 && vwTop > -distance + horizontalSection.getBoundingClientRect().width){
+      horizontalScroll.style.transform = `translateX(${vwTop}px)`;
+    }
+  })
+
+  window.addEventListener('resize', () => {
+    distance = horizontalScroll.scrollWidth;
+    horizontalSection.style.height = `${distance}px`;
+});
 
 
  
